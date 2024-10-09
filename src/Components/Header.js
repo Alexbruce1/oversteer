1fbe0c59b46a1aa9d1a806b0ffca6f5c85299dd9
Alex Bruce <brucealmightyalex@yahoo.com>
2024-10-09 17:22:20 -0600

    Update header a bit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,12 @@ function Header() {
   return (
     <header className="Header">
       <div className="top-header">
-        oversteer
+        <a href="/" className="app-title">
+          over<b>steer</b>
+        </a>
+      </div>
+      <div className="link-container">
+        <a href="/" className="header-link">Home</a>
       </div>
     </header>
   )
