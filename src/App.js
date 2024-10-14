ff8cd2eda1d032bf4a60f5f2b0d22112442fc608
Alex Bruce <brucealmightyalex@yahoo.com>
2024-10-13 19:25:41 -0600

    Add localstorage support to ease api load

--- src/App.js
+++ src/App.js
@@ -7,53 +7,76 @@ import Header from './Components/Header';
 import Drivers from './Components/Drivers';
 import Home from './Components/Home';
 
-const thisYear = new Date().getFullYear()
+const thisYear = new Date().getFullYear();
 
 function App() {
   const [standings, setStandings] = useState([]);
   const [seasons, setSeasons] = useState([]);
-  const [season, setSeason] = useState(thisYear);
+  const [season, setSeason] = useState();
   const [driverImages, setDriverImages] = useState([]);
 
   useEffect(() => {
-    let years = []
-    for (let i = 1950; i <= thisYear; i ++) {
-      years.push(parseInt(i))
+    let years = [];
+    for (let i = 1950; i <= thisYear; i++) {
+      years.push(i);
     }
-    setSeasons(years)
-    console.log(years);
+    setSeasons(years);
+
+    const storedSeason = JSON.parse(localStorage.getItem("Season")) || thisYear;
+    setSeason(storedSeason);
+
+    const storedStandings = JSON.parse(localStorage.getItem("Standings"));
+    const storedImages = JSON.parse(localStorage.getItem("Images"));
+
+    if (storedStandings) {
+      setStandings(storedStandings);
+    } else {
+      fetchStandings(storedSeason);
+    }
+
+    if (storedImages) {
+      setDriverImages(storedImages);
+    }
   }, []);
 
-  useEffect(() => {
-    const fetchStandings = async () => {
-      try {
-        const standingsData = await getStandings(season);
-        setStandings(standingsData);
+  const fetchStandings = async (season) => {
+    try {
+      const standingsData = await getStandings(season);
+      localStorage.setItem("Standings", JSON.stringify(standingsData));
+      setStandings(standingsData);
 
-        const imagePromises = standingsData.map(async (driver) => {
-          const fullName = driver.Driver.familyName == "Sainz" ? `${driver.Driver.givenName}_${driver.Driver.familyName}_jr` : `${driver.Driver.givenName}_${driver.Driver.familyName}`;
-          const imageUrl = await getDriverImage(fullName); // Fetch image from theSportsDB
-          return { driverId: driver.Driver.driverId, imageUrl };
-        });
+      const imagePromises = standingsData.map(async (driver) => {
+        const fullName = driver.Driver.familyName === "Sainz" ? `${driver.Driver.givenName}_${driver.Driver.familyName}_jr` : `${driver.Driver.givenName}_${driver.Driver.familyName}`;
+        const imageUrl = await getDriverImage(fullName);
+        return { driverId: driver.Driver.driverId, imageUrl };
+      });
 
-        const images = await Promise.all(imagePromises);
-        const imagesMap = images.reduce((acc, { driverId, imageUrl }) => {
-          acc[driverId] = imageUrl;
-          return acc;
-        }, {});
-        console.log(imagesMap)
-        setDriverImages(imagesMap);
+      const images = await Promise.all(imagePromises);
+      const imagesMap = images.reduce((acc, { driverId, imageUrl }) => {
+        acc[driverId] = imageUrl;
+        return acc;
+      }, {});
 
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchStandings();
-  }, [season]);
+      setDriverImages(imagesMap);
+      localStorage.setItem("Images", JSON.stringify(imagesMap));
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const chooseSeason = (value) => {
-    setSeason(value)
-  }
+    localStorage.setItem("Season", JSON.stringify(value));
+    setSeason(value);
+
+    const storedStandings = JSON.parse(localStorage.getItem("Standings"));
+
+    if (!storedStandings) {
+      fetchStandings(value);
+    } else {
+      setStandings(storedStandings);
+    }
+  };
 
   return (
     <div className="App">
