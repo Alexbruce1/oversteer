445b3384b6b4df5da07c3334d0feae1211be8768
Alex Bruce <brucealmightyalex@yahoo.com>
2024-10-15 14:15:20 -0600

    Improve API logic

---
+++ src/Components/DriverInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DriverInfo() {
+  return (
+    <div>
+      DRIVER INFO
+    </div>
+  )
+}
+
+export default DriverInfo;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
 import './Home.css';
 
-function Home({ seasons, chooseSeason }) {
+function Home({ seasons, chooseSeason, season }) {
   return (
     <div className="Home">
       <h1 className="home-header">oversteer</h1>
       <form className="home-form">
-        <select className="seasons-dropdown" onChange={(e) => {chooseSeason(e.currentTarget.value)}}>
+        <select 
+          className="seasons-dropdown" 
+          value={season}
+          onChange={(e) => {chooseSeason(e.currentTarget.value)}}>
           {seasons && seasons.length > 0 ? (
             seasons.map(year => (
-              // <option key={year.season} className="season-option">{year.season}</option>
-              <option key={year.season} className="season-option">{year}</option>
+              <option 
+                key={year} 
+                className="season-option">{year}</option>
             ))
           ) : (
             <option>Loading...</option>
