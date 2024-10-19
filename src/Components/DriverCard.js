aa10f3852813128a5696156cfab70999e6510e29
Alex Bruce <brucealmightyalex@yahoo.com>
2024-10-19 10:37:32 -0600

    Update classNames and image for the team version of the cards

--- src/Components/DriverCard.js
+++ src/Components/DriverCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./DriverCard.css"
 import { Link, useLocation } from "react-router-dom";
 
-function DriverCard({ driverKey, driverNumber, driverCode, driverWikiLink, driverImages, driverFirst, driverLast, driverTeam, driverPoints, }) {
+function DriverCard({ driverKey, driverNumber, driverCode, driverWikiLink, driverImages, driverFirst, driverLast, driverTeam, driverPoints, teamPosition, teamPoints, teamName, teamWins, teamNationality }) {
   const location = useLocation();
 
   return (
@@ -12,7 +12,7 @@ function DriverCard({ driverKey, driverNumber, driverCode, driverWikiLink, drive
           to={`/driver/${encodeURIComponent(driverFirst)}_${encodeURIComponent(driverLast)}`}
           className="driver-card-link"
           key={driverKey}>
-          <div className="driver-top-info">
+          <div className="card-top-info">
             <h2 className="driver-number">{driverNumber}</h2>
             <h2 className="driver-code">{driverCode}</h2>
           </div>
@@ -39,32 +39,30 @@ function DriverCard({ driverKey, driverNumber, driverCode, driverWikiLink, drive
       )}
       {(location.pathname === '/teams') && (
         <Link 
-          to={`/driver/${encodeURIComponent(driverFirst)}_${encodeURIComponent(driverLast)}`}
+          to={`/driver/${encodeURIComponent(driverFirst)}`}
           className="driver-card-link"
           key={driverKey}>
-          <div className="driver-top-info">
-            <h2 className="driver-number">{driverNumber}</h2>
-            <h2 className="driver-code">{driverCode}</h2>
+          <div className="card-top-info">
+            <h2 className="team-position">{teamPosition}</h2>
+            <h2 className="team-points">{teamPoints}</h2>
           </div>
           <div 
-            className="team-image-container" 
-            href={driverWikiLink}
-            target="_blank">
+            className="team-image-container">
             { driverImages ? (
               <img 
                 src={driverImages.logo}
-                className="car-image"
-                alt={`${driverFirst} ${driverLast} image`}
+                className="team-image"
+                alt={`${teamName} logo`}
               />
             ) : (
               <div className="no-driver-image no-team-image">no image available</div>
             )}
           </div>
-          <h2 className="driver-name driver-first-name">
-            {driverFirst}  <strong className="driver-name driver-last-name">{driverLast}</strong>
+          <h2 className="team-name">
+            {teamName}
           </h2>
-          <div className="driver-team">{driverTeam}</div>
-          <div className="driver-points">{driverPoints}</div>
+          <div className="team-wins">{teamWins}</div>
+          <div className="team-nationality">{teamNationality}</div>
         </Link>
       )}
 
